8f7a887d5c5178fbc828a46053fa7adc4bb909d3
Jamal Frank <jamaljfrank@gmail.com>
2020-02-09 02:28:10 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,8 +11,8 @@ class Application
         resp.write "#{item}\n"
       end
     elsif req.path.match(/cart/)
-      @@cart.empty? ? resp.write "Your cart is empty" : @@cart.each do |item|
-        resp.write "#{item}\n"
+      @@cart.empty? ? resp.write "Your cart is empty" : @@cart.each do |item| 
+        
       end  
     elsif req.path.match(/search/)
       search_term = req.params["q"]
