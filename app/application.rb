999876123cdcc14c1f77a3094efa812f9fc149d7
Jamal Frank <jamaljfrank@gmail.com>
2020-02-09 02:28:22 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,8 @@ class Application
         resp.write "#{item}\n"
       end
     elsif req.path.match(/cart/)
-      @@cart.empty? ? resp.write "Your cart is empty" : @@cart.each do |item| 
+      @@cart.empty? ? resp.write "Your cart is empty" : @@cart.each do |item| resp.write "#{item}\n"
+    end
         
       end  
     elsif req.path.match(/search/)
