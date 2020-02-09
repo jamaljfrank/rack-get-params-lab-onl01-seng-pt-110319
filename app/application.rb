23aa5fc314f095ce576f7588338c23c2d0b821fb
Jamal Frank <jamaljfrank@gmail.com>
2020-02-09 02:34:02 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
-    elsif
+    elsif 
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
